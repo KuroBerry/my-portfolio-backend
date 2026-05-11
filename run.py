a436ca6b6b2aea572cb47a348da0525970593ccb
ThinhLNQ <dhdncndhxn650@gmail.com>
2026-05-11 17:43:20 +0700

    update

--- run.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-import os
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
-    # Run the app instance from the app package
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
